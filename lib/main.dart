c3b1e41a8c1c405f1b9b7cc3218a3268a7003405
qvistary <qvistary@gmail.com>
2019-06-14 20:17:58 +0800

    change

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("data"),
-        ),
-        body: ListViewDemo(),
-      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.green)
     );
   }
 }
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("data"),
+    ));
+  }
+}
